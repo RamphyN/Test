8c4d8b4d77a3a0aae2a7d7cd92730a06f71587dd
RamphyN <80077894+RamphyN@users.noreply.github.com>
2024-03-04 23:42:31 -0500

    Add files via upload

---
+++ nugget.cs
@@ -0,0 +1,3 @@
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Oracle.EntityFrameworkCore --version 8.21.121
+dotnet add package Oracle.ManagedDataAccess.Core --version 3.21.130
